718ea9e705c02bed425596cd783281affb96481c
Klemen03 <klemenkrklec10@gmail.com>
2024-05-17 10:36:33 +0200

    Build the header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+import { BiCameraMovie } from 'react-icons/bi';
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <div className="flex justify-between p-3 items-center  max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <MenuItem title="home" address="/" Icon={AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <Link href="/" className="flex gap-1 items-center">
+        <BiCameraMovie className="text-4xl hidden sm:inline" />
+        <span className="text-3xl font-bold bg-amber-500 py-1 px-2 rounded-lg   ">
+          WatchMe.
+        </span>
+      </Link>
+    </div>
+  );
+}
+
+export default Header;
