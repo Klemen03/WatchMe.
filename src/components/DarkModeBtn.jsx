0551616e638d8284bf1da3c4fe1c022dede08cee
Klemen03 <klemenkrklec10@gmail.com>
2024-05-17 11:36:56 +0200

    Implemented the dark mode theme

---
+++ src/components/DarkModeBtn.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+import { useTheme } from 'next-themes';
+
+function DarkModeBtn() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  return (
+    <div>
+      {currentTheme === 'dark' ? (
+        <MdLightMode
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme('light')}
+        />
+      ) : (
+        <MdDarkMode
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme('dark')}
+        />
+      )}
+    </div>
+  );
+}
+
+export default DarkModeBtn;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import { BiCameraMovie } from 'react-icons/bi';
 import Link from 'next/link';
+import DarkModeBtn from './DarkModeBtn';
 
 function Header() {
   return (
@@ -12,12 +13,15 @@ function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href="/" className="flex gap-1 items-center">
-        <BiCameraMovie className="text-4xl hidden sm:inline" />
-        <span className="text-3xl font-bold bg-amber-500 py-1 px-2 rounded-lg   ">
-          WatchMe.
-        </span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkModeBtn />
+        <Link href="/" className="flex gap-1 items-center">
+          <BiCameraMovie className="text-4xl hidden sm:inline" />
+          <span className="text-3xl font-bold bg-amber-500 py-1 px-2 rounded-lg   ">
+            WatchMe.
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
