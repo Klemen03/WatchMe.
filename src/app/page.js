b5f712aceeae4471539a486cb8657835c95366dd
Klemen03 <klemenkrklec10@gmail.com>
2024-06-02 21:35:33 +0200

    added search box

--- src/app/page.js
+++ src/app/page.js
@@ -11,13 +11,13 @@ export default async function Home({ searchParams }) {
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page:1`
   );
+  // console.log(res);
   const data = await res.json();
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-  const results = data.results;
-  // console.log(results);
+  // if (!res.ok) {
+  //   throw new Error('Failed to fetch data');
+  // }
+  const results = data.results ? data.results : [];
 
   return (
     <div>
