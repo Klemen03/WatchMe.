4945af604a81fa85f6bfad910c717a081e55a8ac
Klemen03 <klemenkrklec10@gmail.com>
2024-06-03 13:36:23 +0200

    fixed again

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import { useRouter } from 'next/navigation';
 
 function SearchBox() {
@@ -13,24 +13,26 @@ function SearchBox() {
   };
 
   return (
-    <form
-      className="flex justify-between px-5 max-w-6xl mx-auto"
-      onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        placeholder="Search keywords..."
-        className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button
-        className="text-amber-600  disabled:text-gray-300"
-        disabled={search === ''}
+    <Suspense>
+      <form
+        className="flex justify-between px-5 max-w-6xl mx-auto"
+        onSubmit={handleSubmit}
       >
-        Search
-      </button>
-    </form>
+        <input
+          type="text"
+          placeholder="Search keywords..."
+          className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="text-amber-600  disabled:text-gray-300"
+          disabled={search === ''}
+        >
+          Search
+        </button>
+      </form>
+    </Suspense>
   );
 }
 
