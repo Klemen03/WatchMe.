09c39c1605918f3cd5ba2ff276347e8412c7490e
Klemen03 <klemenkrklec10@gmail.com>
2024-06-03 13:31:00 +0200

    fixes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 const API_KEY = process.env.API_KEY;
 
 import Results from '@/components/Results';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
@@ -20,8 +20,8 @@ export default async function Home({ searchParams }) {
   const results = data.results ? data.results : [];
 
   return (
-    <div>
+    <Suspense>
       <Results results={results} />
-    </div>
+    </Suspense>
   );
 }
